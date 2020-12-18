5c3595ea77bb00b5de7b2e6a1fa82c5003ca06e2
Levent007 <levent.mehmed@student.kdg.be>
2020-12-18 14:23:26 +0100

    Heb al wa vooruitgang dus bekijk maar... Kom samen coderen plssss

--- GuesWho/src/wieIsHet/Spel.java
+++ GuesWho/src/wieIsHet/Spel.java
@@ -3,29 +3,26 @@ package wieIsHet;
 import java.util.Random;
 
 public class Spel {
-    private int maxScore;
+    private int score;
     private Speler playerOne;
     private String vraag;
     private Random rand = new Random();
     private int random = rand.nextInt(12)+1;
 
     public Spel() {
-
     }
 
     public int getMaxScore(Speler player) {
-        return this.maxScore;
+        return this.score;
     }
 
     public void setMaxScore(int score) {
-        if ((this.maxScore < score)) {
-            this.maxScore = score;
+        if ((this.score < score)) {
+            this.score = score;
         }
     }
 
-    public void setPlayerOne(Speler player) {
-        this.playerOne = player;
-    }
+
 
 
 
@@ -34,8 +31,8 @@ public class Spel {
     }
 
     public void startSpel(String naam, String personage) {
-        Speler newPlayer = new Speler(naam, 0, personage);
-        setPlayerOne(newPlayer);
+        Speler newPlayer = new Speler(naam, 0);
+        //setPlayerOne(newPlayer);
         setMaxScore(getMaxScore(newPlayer));
     }
 
--- GuesWho/src/wieIsHet/Speler.java
+++ GuesWho/src/wieIsHet/Speler.java
@@ -3,23 +3,33 @@ package wieIsHet;
 public class Speler {
     private String naam;
     private int score;
-    private String vraag;
 
-    public Speler(String naam, int score, String vraag) {
+    public Speler(String naam, int score) {
         this.naam = naam;
-        this.score = score;
-        this.vraag = vraag;
-    }
-
-    public void updateScore(int n) {
-        this.score = this.score + n;
-    }
-
-    public int getScore() {
-        return this.score;
+        this.score = 0;
     }
 
     public String getNaam() {
-        return this.naam;
+        return naam;
+    }
+
+    public void setNaam(String naam) {
+        this.naam = naam;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+
+    @Override
+    public String toString() {
+        return "Speler{" +
+                "naam='" + naam + '\'' +
+                ", score=" + score +
+                '}';
     }
 }
