1d51e416949c1df8cb20750d107e837f48a9136d
Levent007 <levent.mehmed@student.kdg.be>
2021-03-10 21:04:25 +0100

    just saving progress m8

--- src/wieIsHet/view/game/GameView.java
+++ src/wieIsHet/view/game/GameView.java
@@ -1,8 +1,13 @@
 package wieIsHet.view.game;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.geometry.Insets;
+import javafx.scene.control.ComboBox;
 import javafx.scene.layout.BorderPane;
 
 public class GameView  extends BorderPane {
+    private ComboBox<String> cbVragen;
     RosterView rosterView;
     // private Node attributen (controls)
 
@@ -15,6 +20,17 @@ public class GameView  extends BorderPane {
     private void initialiseNodes() {
 
         rosterView = new RosterView();
+
+
+        this.cbVragen= new ComboBox<>();
+        ObservableList<String> namen = FXCollections.observableArrayList(
+                "Mijn vraag dat hier zal komen is wel wat lang denk ik he ?",
+                "Is je haar kleur deze kleur of dat kleur ?",
+                "Wtf Zeg Gij Moat?"
+        );
+        this.cbVragen.setItems(namen);
+        this.cbVragen.getSelectionModel().select(0);
+
         // Initialisatie van de Nodes
         // bvb.:
         // button = new Button("...")
@@ -23,6 +39,8 @@ public class GameView  extends BorderPane {
 
     private void layoutNodes() {
         this.setCenter(rosterView);
+        this.setBottom(this.cbVragen);
+        this.setMargin(this.cbVragen, new Insets(15));
         // Layout van de Nodes
         // add… methodes (of set…)
         // Insets, padding, alignment, …
--- src/wieIsHet/view/game/RosterView.java
+++ src/wieIsHet/view/game/RosterView.java
@@ -1,5 +1,7 @@
 package wieIsHet.view.game;
 
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.geometry.HPos;
 import javafx.geometry.Insets;
 import javafx.scene.control.Button;
@@ -21,6 +23,8 @@ public class RosterView extends GridPane {
 
     private void initialiseNodes() {
         // attributen initialiseren
+        int row = 0;
+        int col = 0;
         for (int i = 0; i < 20; i++) {
             String img = "Person"+i;
             // DEBUGING WITH ONLY ONE PERSON
@@ -33,10 +37,21 @@ public class RosterView extends GridPane {
             ImageView personImage = new ImageView(new Image(loc));
 
             button.setGraphic(personImage);
+            if(row == 4) {
+                col++;
+                row=0;
+            }
+            button.setOnAction(new EventHandler<ActionEvent>() {
+                @Override public void handle(ActionEvent e) {
+                    System.out.print(button+"\n");
+                }
+            });
+            this.add(button, row, col);
+            System.out.print("The ROW: "+row+"\n");
+            System.out.print("The COL: "+col+"\n");
+            row++;
 
-            buttonlist.add(button);
         }
-        System.out.println(buttonlist);
 
     }
 
@@ -46,7 +61,8 @@ public class RosterView extends GridPane {
         this.setHgap(50);
         this.setVgap(35);
         this.setPadding(new Insets(15, 15, 10, 15));
-        this.getChildren().addAll(buttonlist);
+        // this.getChildren().addAll(buttonlist);
     }
+
 
 }
